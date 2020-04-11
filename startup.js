344bc9d8f6b8bcb3fa12704bd854c82decc5730f
AkhilYerraRutgers <ay260@scarletmail.rutgers.edu>
2020-04-11 17:12:28 -0400

    dev stuff

--- startup.js
+++ startup.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose')
 exports.startUp = () =>{
 //DEV MONGO :  mongodb+srv://akhilPoker:Laser360@cluster0-tme7p.gcp.mongodb.net/test?retryWrites=true&w=majority
 //LOCAL MONGO: mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false
-var mongoDB = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+var mongoDB = 'mongodb+srv://akhilPoker:Laser360@cluster0-tme7p.gcp.mongodb.net/test?retryWrites=true&w=majority';
 try{
     mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
     console.log("Connected To Mongo");
