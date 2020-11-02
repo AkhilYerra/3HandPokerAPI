6dc4ba0a8be273649a99d9718f20206731ea42b5
AkhilYerra <akhil.yerra09@gmail.com>
2020-11-02 18:44:05 -0500

    Update startup.js

--- startup.js
+++ startup.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 
 exports.startUp = () =>{
 //TODO:CHANGE
-//DEV MONGO :  mongodb+srv://akhilPoker:Laser360@cluster0-tme7p.gcp.mongodb.net/test?retryWrites=true&w=majority
+//DEV MONGO :  mongodb+srv://*******:******@cluster0-tme7p.gcp.mongodb.net/test?retryWrites=true&w=majority
 //LOCAL MONGO: mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false
 var mongoDB = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
 console.log(mongoDB);
